4f622906a2de91a1c5bdf1cbd05e63b60da6d92a
johnfoley <john@babblebat.com>
2018-03-06 14:00:14 -0500

    Add short cuts for other apps

--- modules/apps.lua
+++ modules/apps.lua
@@ -11,6 +11,15 @@ function appsModule.init()
     hotkey.bind(mash, 'T', function()
     	hs.application.launchOrFocus("Terminal")
     end)
+    hotkey.bind(mash, 'X', function()
+    	hs.application.launchOrFocus("XCode")
+    end)
+    hotkey.bind(mash, 'I', function()
+    	hs.application.launchOrFocus("IntelliJ IDEA")
+    end)
+    hotkey.bind(mash, 'A', function()
+    	hs.application.launchOrFocus("AppCode")
+    end)
 end
 
 return appsModule 
